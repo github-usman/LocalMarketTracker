6ce1cdd149e2372cf6cf95cbcbb75913e86be9a4
github-usman <usmantoyou@gmail.com>
2024-03-13 13:00:39 +0530

    fronend : hamburger menu added and device navbar initialized

--- frontend/src/layouts/header/HeaderLayout.js
+++ frontend/src/layouts/header/HeaderLayout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/Navbar.js";
 import AllDepartments from "../../components/all-departments/AllDepartments.js";
 import "./header.css";
@@ -6,22 +6,66 @@ import SearchBox from "../../components/search/SearchBox.js";
 import Logo from "../../components/logo/Logo";
 import AuthLayout from "../../components/auth/AuthLayout.js";
 import CitySearchBox from "../../components/search-by-city/CitySearchBox.js";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 
 const HeaderLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handHambureger = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>
-      <div className="search-section">
-        <Logo />
-        <Navbar />
-        <AuthLayout />
+    <>
+      <div className="headerLayout-lg-device">
+        <div className="nav-section">
+          <Logo />
+          <Navbar />
+          <AuthLayout />
+        </div>
+        <div className="search-section">
+          <AllDepartments />
+          <SearchBox />
+          <CitySearchBox></CitySearchBox>
+        </div>
+        <hr></hr>
       </div>
-      <div className="nav-section">
-        <AllDepartments />
-        <SearchBox />
-        <CitySearchBox></CitySearchBox>
+      <div className="headerLayout-md-device">
+        <div className="nav-section">
+          <Logo />
+          <AllDepartments />
+          <AuthLayout />
+        </div>
+        <div className="search-section">
+          <Navbar />
+          <SearchBox />
+          <CitySearchBox></CitySearchBox>
+        </div>
+      </div>
+      <div className="headerLayout-sm-device">
+        <div className="nav-section">
+          <div className="logo-sm-section">
+            <Logo />
+            {isOpen ? (
+              <RxCross2 className="hamburger" onClick={handHambureger} />
+            ) : (
+              <RxHamburgerMenu className="hamburger" onClick={handHambureger} />
+            )}
+          </div>
+          {isOpen && (
+            <>
+              <Navbar />
+              <AuthLayout />
+            </>
+          )}
+        </div>
+        <div className="search-section">
+          <AllDepartments />
+          <SearchBox />
+          <CitySearchBox></CitySearchBox>
+        </div>
       </div>
       <hr></hr>
-    </div>
+    </>
   );
 };
 
