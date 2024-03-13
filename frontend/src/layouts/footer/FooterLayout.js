1ba86351566f44111557d6e3a5e3fcc413c08087
github-usman <usmantoyou@gmail.com>
2024-03-13 18:55:52 +0530

    frontend : random products add

--- frontend/src/layouts/footer/FooterLayout.js
+++ frontend/src/layouts/footer/FooterLayout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./footerlayout.css"
 import FooterProductLinks from '../../components/footer/FooterProductLinks'
 const FooterLayout = () => {
+  
   return (
     <div className='footer-layout-container'>
         <h2 style={{textAlign:"center",backgroundColor:"#e2d348"}}>! This project is Ongoing...Please try after some time -by Usman</h2>
