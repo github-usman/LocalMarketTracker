2276d0aedff92c37dd717f4dff3fe671743bb485
github-usman <usmantoyou@gmail.com>
2024-03-13 11:48:06 +0530

    frontend : scroll-bar for city and universal scroll

--- frontend/src/components/search/SearchBox.js
+++ frontend/src/components/search/SearchBox.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import "./search.css";
-import { CiSearch } from "react-icons/ci";
+import { GoSearch } from "react-icons/go";
 
 const SearchBox = () => {
   const [value, setValue] = useState("")
   return (
     <div className='search-container'>
         <input type='text' value={value} className='input-btn' placeholder='Search for products' onChange={e=>setValue(e.target.value)} /> 
-        <button className='search-btn'><CiSearch className='search-icon'/></button>
+        <button className='search-btn'><GoSearch className='search-icon'/></button>
     </div>
   )
 }
