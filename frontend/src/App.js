3987900d694d1ed9e977abaa0ab9352ac1670a26
github-usman <usmantoyou@gmail.com>
2024-03-14 10:37:46 +0530

    Frontend : Webpack + babel configuration setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import { router } from "./routes/routes";
 import { RouterProvider } from "react-router-dom";
 import "./assests/styles/global.css";
 import Loader from "./components/spinner/Loader";
+
 function App() {
   return (
     <div className="App">
       <Suspense fallback={<Loader/>}>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </Suspense>
     </div>
   );
---
+++ frontend/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        // filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jp(e*)g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "fonts/",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+    devServer: {
+        port: 3000,
+    },
+};
